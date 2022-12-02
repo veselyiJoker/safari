b99681a7578784c407d4035c26d107635a7fcef8
Yahor Antushevich <eantushevich@twelvedevs.com>
2022-12-02 16:58:36 +0300

    fdsf

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { default: webViewJavaScriptBridge } = require("webview-javascript-bridge");
+
 // http://stackoverflow.com/questions/24571548/javascript-how-to-detect-if-the-custom-url-scheme-is-available-or-not-availabl
 function goto(url, fallback) {
     var script = document.createElement('script'); 
@@ -12,3 +14,5 @@ function goto(url, fallback) {
 
     document.getElementsByTagName('head')[0].appendChild(script);
 }
+
+self.close();
