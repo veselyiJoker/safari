944ed54684c475ac31703a4792c76743b220f152
Yahor Antushevich <eantushevich@twelvedevs.com>
2022-12-02 16:37:41 +0300

    fdsf

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-import webViewJavaScriptBridge from "webview-javascript-bridge";
+// http://stackoverflow.com/questions/24571548/javascript-how-to-detect-if-the-custom-url-scheme-is-available-or-not-availabl
+function goto(url, fallback) {
+    var script = document.createElement('script'); 
 
-async function sendingMessage() {
-    let response = await webViewJavaScriptBridge
-      .sendMessage({
-        action: "tester",
-        params: 123456,
-      })
-    console.log("tester's response", response)
+    script.onload = function() { 
+        document.location = url;
+    } 
+    script.onerror = function() { 
+        document.location = fallback;
+    } 
+    script.setAttribute('src', url); 
+
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
-
-sendingMessage()
